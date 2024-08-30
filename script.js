b9b75f0ba2874bd5b9743b6c165e176b379df082
H. Walid <h.walid@hexaee.com>
2024-08-30 21:01:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+document.addEventListener("mousemove", function (e) {
+  const svg = document.querySelector(".rotate-svg");
+  const svgRect = svg.getBoundingClientRect();
+
+  const svgCenterX = svgRect.left + svgRect.width / 2;
+  const svgCenterY = svgRect.top + svgRect.height / 2;
+
+  const angle =
+    Math.atan2(e.clientY - svgCenterY, e.clientX - svgCenterX) *
+    (180 / Math.PI);
+
+  svg.style.transform = `rotate(${angle}deg)`;
+});
+
+document.addEventListener("mousemove", function (e) {
+  const element = document.querySelector(".tr");
+  const cursorX = e.clientX;
+  const cursorY = e.clientY;
+
+  const rect = element.getBoundingClientRect();
+  const elementX = rect.left + rect.width / 2;
+  const elementY = rect.top + rect.height / 2;
+
+  const deltaX = cursorX - elementX;
+  const deltaY = cursorY - elementY;
+  const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+  element.style.transform = `translate(1000px, 200px) rotate(${angle}deg)`;
+});
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+  //  cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    { left: `${posX}px`, top: `${posY}px` },
+    { duration: 500, fill: "forwards" }
+  );
+});
+
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
